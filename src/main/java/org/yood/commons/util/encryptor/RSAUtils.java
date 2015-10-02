dcac4bd1f60c189b9771db55d890080c5e46e462
Shenjian YUAN <sjyuan@thoughtworks.com>
2015-10-02 22:35:14 +0800

    add test for rsa

--- src/main/java/org/yood/commons/util/encryptor/RSAUtils.java
+++ src/main/java/org/yood/commons/util/encryptor/RSAUtils.java
@@ -1,10 +1,13 @@
 package org.yood.commons.util.encryptor;
 
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.crypto.Cipher;
 import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class RSAUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(RSAUtils.class);
@@ -21,7 +24,29 @@ public class RSAUtils {
         }
     }
 
-    public static byte[] encrypt(String data, PublicKey publicKey) {
+    public static PublicKey getPublicKey(String key) throws Exception {
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.decodeBase64(key));
+        KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+        return publicKey;
+    }
+
+    public static String getPublicKey(PublicKey key) {
+        return Base64.encodeBase64String(key.getEncoded());
+    }
+
+    public static PrivateKey getPrivateKey(String key) throws Exception {
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(key));
+        KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+        return privateKey;
+    }
+
+    public static String getPrivateKey(PrivateKey key) {
+        return Base64.encodeBase64String(key.getEncoded());
+    }
+
+    public static byte[] encryptAsByteArray(String data, PublicKey publicKey) {
         try {
             Cipher cipher = Cipher.getInstance(ALGORITHM);
             cipher.init(Cipher.ENCRYPT_MODE, publicKey);
@@ -39,5 +64,13 @@ public class RSAUtils {
         } catch (Exception e) {
             throw new IllegalArgumentException("Decrypt failed!", e);
         }
+    }
+
+    public static String encryptAsString(String data, PublicKey publicKey) {
+        return Base64.encodeBase64String(encryptAsByteArray(data, publicKey));
+    }
+
+    public static String decrypt(String data, PrivateKey privateKey) {
+        return decrypt(Base64.decodeBase64(data), privateKey);
     }
 }
--- src/test/java/org/yood/commons/util/encryptor/RSAUtilsTest.java
+++ src/test/java/org/yood/commons/util/encryptor/RSAUtilsTest.java
@@ -1,5 +1,7 @@
 package org.yood.commons.util.encryptor;
 
+import junit.framework.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,36 +11,79 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
 
 public class RSAUtilsTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RSAUtilsTest.class);
 
+    private static KeyPair keyPair;
+    private static PublicKey publicKey;
+    private static PrivateKey privateKey;
+
+    @BeforeClass
+    public static void setup() {
+        keyPair = RSAUtils.generateKeyPair();
+        publicKey = keyPair.getPublic();
+        privateKey = keyPair.getPrivate();
+        LOGGER.info("Public Key = {}, Private Key = {}", publicKey, privateKey);
+    }
+
     @Test
     public void testGenerateKeyPair() throws Exception {
-        KeyPair keyPair = RSAUtils.generateKeyPair();
-        assertNotNull(keyPair);
-
+        Assert.assertNotNull(keyPair);
+        Assert.assertNotNull(publicKey);
+        Assert.assertNotNull(privateKey);
     }
 
     @Test
-    public void testEncrypt() throws Exception {
-        KeyPair keyPair = RSAUtils.generateKeyPair();
-        PublicKey publicKey = keyPair.getPublic();
-        PrivateKey privateKey = keyPair.getPrivate();
-        LOGGER.info("Public Key = {}, Private Key = {}", publicKey, privateKey);
+    public void testGetPublicKey() throws Exception {
+        String publicKeyString = RSAUtils.getPublicKey(RSAUtilsTest.publicKey);
+        assertEquals(publicKey, RSAUtils.getPublicKey(publicKeyString));
+    }
 
+    @Test
+    public void testGetPublicKey1() throws Exception {
+        testGetPublicKey();
+    }
+
+    @Test
+    public void testGetPrivateKey() throws Exception {
+        String privateKeyString = RSAUtils.getPrivateKey(RSAUtilsTest.privateKey);
+        assertEquals(privateKey, RSAUtils.getPrivateKey(privateKeyString));
+    }
+
+    @Test
+    public void testGetPrivateKey1() throws Exception {
+        testGetPrivateKey();
+    }
+
+    @Test
+    public void testEncryptAsByteArray() throws Exception {
         String encryptData = "中华人民共和国";
         LOGGER.info("Encrypt data = {}", encryptData);
-        byte[] encryptedData = RSAUtils.encrypt(encryptData, publicKey);
+        byte[] encryptedData = RSAUtils.encryptAsByteArray(encryptData, publicKey);
         String decryptData = RSAUtils.decrypt(encryptedData, privateKey);
         LOGGER.info("Decrypt data = {}", decryptData);
-        assertEquals(encryptData,decryptData);
+        assertEquals(encryptData, decryptData);
     }
 
     @Test
-    public void testDecrypt() throws Exception {
-        testEncrypt();
+    public void testEncryptAsString() throws Exception {
+        String encryptData = "中华人民共和国";
+        LOGGER.info("Encrypt data = {}", encryptData);
+        String encryptedData = RSAUtils.encryptAsString(encryptData, publicKey);
+        String decryptData = RSAUtils.decrypt(encryptedData, privateKey);
+        LOGGER.info("Decrypt data = {}", decryptData);
+        assertEquals(encryptData, decryptData);
+    }
+
+    @Test
+    public void testDecryptForByteArray() throws Exception {
+        testEncryptAsByteArray();
+    }
+
+    @Test
+    public void testDecryptForString() throws Exception {
+        testEncryptAsString();
     }
 }

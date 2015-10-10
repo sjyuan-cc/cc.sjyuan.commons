6d37c9563e4d97364aefe73d37baedb729bc8301
Shenjian YUAN <sjyuan@thoughtworks.com>
2015-10-10 09:52:30 +0800

    refactor RSAUtils

--- src/main/java/org/yood/commons/util/encryptor/RSAUtils.java
+++ src/main/java/org/yood/commons/util/encryptor/RSAUtils.java
@@ -1,54 +1,96 @@
 package org.yood.commons.util.encryptor;
 
 import org.apache.commons.codec.binary.Base64;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.crypto.Cipher;
+import java.math.BigInteger;
 import java.security.*;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
 public class RSAUtils {
-    private static final Logger LOGGER = LoggerFactory.getLogger(RSAUtils.class);
+
+    public static final String RSA_ECB_PKCS1_PADDING = "RSA/ECB/PKCS1Padding";
+
+    public static final int KEY_SIZE_2048 = 2048;
+    public static final int KEY_SIZE_1024 = 1024;
+
+    private RSAUtils() {
+    }
 
     private static final String ALGORITHM = "RSA";
 
     public static KeyPair generateKeyPair() {
+        return generateKeyPair(KEY_SIZE_2048);
+    }
+
+    public static KeyPair generateKeyPair(int keySize) {
         try {
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(ALGORITHM);
-            keyPairGenerator.initialize(2048);
+            keyPairGenerator.initialize(keySize);
             return keyPairGenerator.generateKeyPair();
         } catch (NoSuchAlgorithmException e) {
             throw new IllegalArgumentException("Failed to generate key pair!", e);
         }
     }
 
-    public static PublicKey getPublicKey(String key) throws Exception {
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.decodeBase64(key));
-        KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
-        PublicKey publicKey = keyFactory.generatePublic(keySpec);
-        return publicKey;
+    public static PublicKey getPublicKey(String base64PublicKey) {
+        try {
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.decodeBase64(base64PublicKey));
+            KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+            PublicKey publicKey = keyFactory.generatePublic(keySpec);
+            return publicKey;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Failed to get public key!", e);
+        }
     }
 
-    public static String getPublicKey(PublicKey key) {
-        return Base64.encodeBase64String(key.getEncoded());
+    public static PublicKey getPublicKey(BigInteger modulus, BigInteger exponent) {
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+            RSAPublicKeySpec keySpec = new RSAPublicKeySpec(modulus, exponent);
+            return keyFactory.generatePublic(keySpec);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public static PrivateKey getPrivateKey(String key) throws Exception {
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(key));
-        KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
-        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
-        return privateKey;
+    public static String getBase64PublicKey(PublicKey publicKey) {
+        return Base64.encodeBase64String(publicKey.getEncoded());
     }
 
-    public static String getPrivateKey(PrivateKey key) {
-        return Base64.encodeBase64String(key.getEncoded());
+    public static PrivateKey getPrivateKey(String base64PrivateKey) {
+        try {
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(base64PrivateKey));
+            KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+            PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+            return privateKey;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Failed to get private key!", e);
+        }
+    }
+
+    public static PrivateKey getPrivateKey(BigInteger modulus, BigInteger exponent) {
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+            RSAPrivateKeySpec keySpec = new RSAPrivateKeySpec(modulus, exponent);
+            return keyFactory.generatePrivate(keySpec);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String getBase64PrivateKey(PrivateKey privateKey) {
+        return Base64.encodeBase64String(privateKey.getEncoded());
     }
 
     public static byte[] encryptAsByteArray(String data, PublicKey publicKey) {
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            Cipher cipher = Cipher.getInstance(RSA_ECB_PKCS1_PADDING);
             cipher.init(Cipher.ENCRYPT_MODE, publicKey);
             return cipher.doFinal(data.getBytes());
         } catch (Exception e) {
@@ -56,9 +98,21 @@ public class RSAUtils {
         }
     }
 
+    public static byte[] encryptAsByteArray(String data, String base64PublicKey) {
+        return encryptAsByteArray(data, getPublicKey(base64PublicKey));
+    }
+
+    public static String encryptAsString(String data, PublicKey publicKey) {
+        return Base64.encodeBase64String(encryptAsByteArray(data, publicKey));
+    }
+
+    public static String encryptAsString(String data, String base64PublicKey) {
+        return Base64.encodeBase64String(encryptAsByteArray(data, getPublicKey(base64PublicKey)));
+    }
+
     public static String decrypt(byte[] data, PrivateKey privateKey) {
         try {
-            Cipher cipher = Cipher.getInstance("RSA");
+            Cipher cipher = Cipher.getInstance(RSA_ECB_PKCS1_PADDING);
             cipher.init(Cipher.DECRYPT_MODE, privateKey);
             return new String(cipher.doFinal(data));
         } catch (Exception e) {
@@ -66,11 +120,16 @@ public class RSAUtils {
         }
     }
 
-    public static String encryptAsString(String data, PublicKey publicKey) {
-        return Base64.encodeBase64String(encryptAsByteArray(data, publicKey));
+    public static String decrypt(byte[] data, String base64PrivateKey) {
+        return decrypt(data, getPrivateKey(base64PrivateKey));
     }
 
     public static String decrypt(String data, PrivateKey privateKey) {
         return decrypt(Base64.decodeBase64(data), privateKey);
     }
+
+    public static String decrypt(String data, String base64PrivateKey) {
+        return decrypt(Base64.decodeBase64(data), getPrivateKey(base64PrivateKey));
+    }
 }
+
--- src/test/java/org/yood/commons/util/encryptor/RSAUtilsTest.java
+++ src/test/java/org/yood/commons/util/encryptor/RSAUtilsTest.java
@@ -1,16 +1,17 @@
 package org.yood.commons.util.encryptor;
 
-import junit.framework.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
 
 public class RSAUtilsTest {
 
@@ -22,44 +23,71 @@ public class RSAUtilsTest {
 
     @BeforeClass
     public static void setup() {
-        keyPair = RSAUtils.generateKeyPair();
+        keyPair = RSAUtils.generateKeyPair(RSAUtils.KEY_SIZE_2048);
         publicKey = keyPair.getPublic();
         privateKey = keyPair.getPrivate();
         LOGGER.info("Public Key = {}, Private Key = {}", publicKey, privateKey);
     }
 
     @Test
-    public void testGenerateKeyPair() throws Exception {
-        Assert.assertNotNull(keyPair);
-        Assert.assertNotNull(publicKey);
-        Assert.assertNotNull(privateKey);
+    public void testGenerateKeyPairWhenDefaultSize() throws Exception {
+        KeyPair keyPair = RSAUtils.generateKeyPair();
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+        assertNotNull(keyPair);
+        assertNotNull(publicKey);
+        assertNotNull(privateKey);
     }
 
     @Test
-    public void testGetPublicKey() throws Exception {
-        String publicKeyString = RSAUtils.getPublicKey(RSAUtilsTest.publicKey);
+    public void testGenerateKeyPair() throws Exception {
+        KeyPair keyPair = RSAUtils.generateKeyPair(RSAUtils.KEY_SIZE_2048);
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+        assertNotNull(keyPair);
+        assertNotNull(publicKey);
+        assertNotNull(privateKey);
+    }
+
+    @Test
+    public void testGetBase64PublicKey() throws Exception {
+        String publicKeyString = RSAUtils.getBase64PublicKey(publicKey);
         assertEquals(publicKey, RSAUtils.getPublicKey(publicKeyString));
     }
 
     @Test
-    public void testGetPublicKey1() throws Exception {
-        testGetPublicKey();
+    public void testGetPublicKey() throws Exception {
+        testGetBase64PublicKey();
     }
 
     @Test
-    public void testGetPrivateKey() throws Exception {
-        String privateKeyString = RSAUtils.getPrivateKey(RSAUtilsTest.privateKey);
+    public void testGetBase64PrivateKey() throws Exception {
+        String privateKeyString = RSAUtils.getBase64PrivateKey(privateKey);
         assertEquals(privateKey, RSAUtils.getPrivateKey(privateKeyString));
     }
 
     @Test
-    public void testGetPrivateKey1() throws Exception {
-        testGetPrivateKey();
+    public void testGetPublicKeyFromNAndEBigInteger() throws Exception {
+        BigInteger modulus = new BigInteger(publicKey.getEncoded());
+        BigInteger exponent = new BigInteger("65573");
+        assertNotNull(RSAUtils.getPublicKey(modulus, exponent));
+    }
+
+    @Test
+    public void testGetPrivateKeyFromNAndEBigInteger() throws Exception {
+        BigInteger modulus = new BigInteger(privateKey.getEncoded());
+        BigInteger exponent = new BigInteger("65573");
+        assertNotNull(RSAUtils.getPrivateKey(modulus, exponent));
+    }
+
+    @Test
+    public void testGetPrivateKey() throws Exception {
+        testGetBase64PrivateKey();
     }
 
     @Test
     public void testEncryptAsByteArray() throws Exception {
-        String encryptData = "中华人民共和国";
+        String encryptData = "Chinese people";
         LOGGER.info("Encrypt data = {}", encryptData);
         byte[] encryptedData = RSAUtils.encryptAsByteArray(encryptData, publicKey);
         String decryptData = RSAUtils.decrypt(encryptedData, privateKey);
@@ -69,7 +97,7 @@ public class RSAUtilsTest {
 
     @Test
     public void testEncryptAsString() throws Exception {
-        String encryptData = "中华人民共和国";
+        String encryptData = "Chinese people";
         LOGGER.info("Encrypt data = {}", encryptData);
         String encryptedData = RSAUtils.encryptAsString(encryptData, publicKey);
         String decryptData = RSAUtils.decrypt(encryptedData, privateKey);
@@ -85,5 +113,25 @@ public class RSAUtilsTest {
     @Test
     public void testDecryptForString() throws Exception {
         testEncryptAsString();
+    }
+
+    @Test
+    public void testEncryptAsString1() throws Exception {
+        String encryptData = "Chinese people";
+        LOGGER.info("Encrypt data = {}", encryptData);
+        String encryptedData = RSAUtils.encryptAsString(encryptData, RSAUtils.getBase64PublicKey(publicKey));
+        String decryptData = RSAUtils.decrypt(encryptedData, RSAUtils.getBase64PrivateKey(privateKey));
+        LOGGER.info("Decrypt data = {}", decryptData);
+        assertEquals(encryptData, decryptData);
+    }
+
+    @Test
+    public void testEncryptAsByteArray1() throws Exception {
+        String encryptData = "Chinese people";
+        LOGGER.info("Encrypt data = {}", encryptData);
+        byte[] encryptedData = RSAUtils.encryptAsByteArray(encryptData, RSAUtils.getBase64PublicKey(publicKey));
+        String decryptData = RSAUtils.decrypt(encryptedData, RSAUtils.getBase64PrivateKey(privateKey));
+        LOGGER.info("Decrypt data = {}", decryptData);
+        assertEquals(encryptData, decryptData);
     }
 }
